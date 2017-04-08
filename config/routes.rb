9c18bc59a4f631fa2336c7a22757120e5c50f1d5
soruma <soruma.net@gmail.com>
2017-04-09 00:35:37 +0900

    home controller 追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
+  root :to => 'home#index'
   scope "/:locale" do
+    get '/ja', to: 'home#index'
+    get '/en', to: 'home#index'
+    get '/info', to: 'home#info'
+
     resources :uses
     resources :accounts
     resources :histories
---
+++ spec/requests/routing_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe HomeController, type: :routing do
+  describe "routing" do
+    it "routes to home#index" do
+      expect(:get => "/").to route_to("home#index")
+    end
+
+    it "routes to home#info" do
+      expect(:get => "/info").to route_to("home#info")
+    end
+
+    it "routes to ja home#info" do
+      expect(:get => "/ja/info").to route_to("home#info", locale: "ja")
+    end
+
+    it "routes to use#import" do
+      expect(:post => "/uses/import").to route_to("uses#import")
+    end
+  end
+end
