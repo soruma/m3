501434d44a746c3b55553196ad3a94943604ba53
soruma <soruma.net@gmail.com>
2017-04-22 11:10:26 +0900

    Histories test completion

--- spec/factories/histories.rb
+++ spec/factories/histories.rb
@@ -5,8 +5,19 @@ FactoryGirl.define do
     price "9.99"
   end
 
+  factory :invalid_history, class: History do
+    date_of_onset nil
+    price nil
+  end
+
+  factory :update_history, class: History do
+    date_of_onset "2017-03-06"
+    association :account, factory: :wallet
+    price "10.99"
+  end
+
   # 2017-01 月分
-  factory :his_2017_01 do
+  factory :his_2017_01, class: History do
     date_of_onset "2017-01-31"
     association :account, factory: :wallet
     price "999"
--- spec/models/history_spec.rb
+++ spec/models/history_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe History, type: :model do
     history = FactoryGirl.build(:history)
     expect(history).to be_valid
   end
+
+  it "is invalid with date_of_onset and account and price" do
+    history = FactoryGirl.build(:invalid_history)
+    expect(history).to be_invalid
+  end
 end
