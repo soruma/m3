16e3bce7ad27671701dedadfa16feb107cd0091e
soruma <soruma.net@gmail.com>
2017-04-26 23:42:59 +0900

    Create bunch actions

---
+++ spec/requests/accounts_routing_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe AccountsController, type: :routing do
+  describe "routing" do
+    it "routes to accounts#bunch" do
+      expect(:post => "/accounts/bunch").to route_to("accounts#bunch")
+    end
+
+    it "routes to accounts#import" do
+      expect(:post => "/accounts/import").to route_to("accounts#import")
+    end
+
+    it "routes to accounts#export" do
+      expect(:post => "/accounts/export").to route_to("accounts#export")
+    end
+  end
+end
