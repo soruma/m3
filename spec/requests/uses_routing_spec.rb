f4767f18fdc814a9d2e24d15684f9e3fdd990f6f
soruma <soruma.net@gmail.com>
2017-05-04 10:55:19 +0900

    Remove bunch test

--- spec/requests/uses_routing_spec.rb
+++ spec/requests/uses_routing_spec.rb
@@ -2,10 +2,6 @@ require 'rails_helper'
 
 RSpec.describe UsesController, type: :routing do
   describe "routing" do
-    it "routes to uses#bunch" do
-      expect(:post => "/uses/bunch").to route_to("uses#bunch")
-    end
-
     it "routes to uses#import" do
       expect(:post => "/uses/import").to route_to("uses#import")
     end
