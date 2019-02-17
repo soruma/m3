765944746c4f4f1439c8dc7cddd9ee8b07027195
soruma <soruma.net@gmail.com>
2019-02-17 17:22:15 +0900

    version up rails 5.2

--- db/schema.rb
+++ db/schema.rb
@@ -10,39 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170305042126) do
+ActiveRecord::Schema.define(version: 2017_03_05_042126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "accounts", force: :cascade do |t|
-    t.integer  "use_id"
-    t.string   "name"
+  create_table "accounts", id: :serial, force: :cascade do |t|
+    t.integer "use_id"
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["use_id"], name: "index_accounts_on_use_id", using: :btree
+    t.index ["use_id"], name: "index_accounts_on_use_id"
   end
 
-  create_table "events", force: :cascade do |t|
-    t.date     "date_of_onset"
-    t.string   "name"
-    t.string   "remarks"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+  create_table "events", id: :serial, force: :cascade do |t|
+    t.date "date_of_onset"
+    t.string "name"
+    t.string "remarks"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table "histories", force: :cascade do |t|
-    t.date     "date_of_onset"
-    t.integer  "account_id"
-    t.decimal  "price"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
-    t.index ["account_id"], name: "index_histories_on_account_id", using: :btree
-    t.index ["date_of_onset", "account_id"], name: "index_histories_on_date_of_onset_and_account_id", unique: true, using: :btree
+  create_table "histories", id: :serial, force: :cascade do |t|
+    t.date "date_of_onset"
+    t.integer "account_id"
+    t.decimal "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["account_id"], name: "index_histories_on_account_id"
+    t.index ["date_of_onset", "account_id"], name: "index_histories_on_date_of_onset_and_account_id", unique: true
   end
 
-  create_table "uses", force: :cascade do |t|
-    t.string   "name"
+  create_table "uses", id: :serial, force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
