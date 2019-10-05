2a114aa0425b624aa26765524b968963431f48b6
soruma <soruma.net@gmail.com>
2019-10-06 02:46:01 +0900

    Fix Style/RescueModifier

--- app/helpers/file_import_helper.rb
+++ app/helpers/file_import_helper.rb
@@ -2,7 +2,11 @@ module FileImportHelper
   #=== file import
   def csv_file_import(upload_file)
     CSV.foreach(upload_file.path, headers: true, encoding: 'Shift_JIS') do |row|
-      obj = find(row['id']) rescue new
+      obj = begin
+              find(row['id'])
+            rescue StandardError
+              new
+            end
       obj.attributes = row.to_hash.slice(*updatable_attributes)
       obj.save!
     end
