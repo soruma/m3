75d1b01ea729e4661a558954096dd8c0d3619c77
soruma <soruma.net@gmail.com>
2019-10-11 08:51:33 +0900

    Install Bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,5 +14,5 @@
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-console.log('Hello World from Webpacker')
+import "bootstrap"
+import "../src/application.scss"
