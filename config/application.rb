f10df39b9a1abf5dce5e770842e7117016e85ce1
soruma <soruma.net@gmail.com>
2019-10-11 19:31:55 +0900

    Move generators to initializers folder

--- config/application.rb
+++ config/application.rb
@@ -24,20 +24,5 @@ module App
     config.i18n.enforce_available_locales = true
     config.i18n.default_locale = :en
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-
-    config.generators do |g|
-      g.javascripts false
-      g.stylesheets false
-      g.helper false
-      g.assets false
-      g.test_framework :rspec,
-                       fixture: true,
-                       view_specs: false,
-                       helper_specs: false,
-                       routing_specs: false,
-                       controller_specs: true,
-                       request_specs: true
-      g.fixture_replacement :factory_bot, dir: 'spec/factories'
-    end
   end
 end
