ba3dfea46356a7dbd095c77336fd464dff6f7e6c
soruma <soruma.net@gmail.com>
2019-10-12 17:03:58 +0900

    Enhanced account validation

--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -3,13 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe Account, type: :model do
-  it 'is valid with name and use' do
-    account = build(:account)
-    expect(account).to be_valid
-  end
+  describe 'Validation' do
+    it 'is valid with name and use' do
+      account = build(:account)
+      expect(account).to be_valid
+    end
 
-  it 'is invalid with name and use' do
-    account = build(:invalid_account)
-    expect(account).to be_invalid
+    it 'is invalid with name' do
+      history = build(:account, name: nil)
+      history.valid?
+      expect(history.errors[:name]).to include(%(can't be blank))
+    end
+
+    it 'is invalid with use' do
+      history = build(:account, use: nil)
+      history.valid?
+      expect(history.errors[:use]).to include('must exist')
+    end
   end
 end
