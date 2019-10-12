a28694db92af68a137d4817e1acc057c96a62e17
soruma <soruma.net@gmail.com>
2019-10-12 17:58:25 +0900

    Implemented pagination in evnet

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -6,7 +6,7 @@ class EventsController < ApplicationController
   # GET /events
   # GET /events.json
   def index
-    @events = Event.all
+    @events = Event.time_series.page(params[:page])
   end
 
   # GET /events/1
