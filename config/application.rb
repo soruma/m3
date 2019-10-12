d9821ccc0834246d3c62a88db22fac6353fd959a
soruma <soruma.net@gmail.com>
2019-10-12 13:22:18 +0900

    rails app:update

--- config/application.rb
+++ config/application.rb
@@ -16,8 +16,10 @@ module App
     config.load_defaults 5.2
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
+
     # locale
     config.time_zone = 'Tokyo'
     config.i18n.available_locales = %i[ja en]
