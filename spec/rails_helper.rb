2190488ed5c8a0f5797b248cd0c07da2eeb098df
soruma <soruma.net@gmail.com>
2019-10-13 20:13:08 +0900

    Move cov settings

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,5 +1,13 @@
 # frozen_string_literal: true
 
+require 'simplecov'
+SimpleCov.start 'rails'
+
+if ENV['CI'] == 'true'
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
--- spec/support/simplecov.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require 'simplecov'
-SimpleCov.start 'rails'
-
-if ENV['CI'] == 'true'
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
-end
