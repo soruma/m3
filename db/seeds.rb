1d49202ec0e85dfbe99b67b2004ac7e82918e3dd
soruma <soruma.net@gmail.com>
2019-12-08 11:08:36 +0900

    Remove unnecessary seed definitions

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,3 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-
-# FactoryGirl
-require 'factory_girl'
-Dir[Rails.root.join('spec', 'factories', '**', '*.rb')].each { |f| require f }
-
-Account.delete_all
-Use.delete_all
-
-FactoryGirl.create(:wallet)
