a6c8a08e96bddd025a653efa2b8439486c7b7073
soruma <soruma.net@gmail.com>
2019-12-09 22:09:10 +0900

    Fix to be batch processing

--- app/models/concerns/to_csv.rb
+++ app/models/concerns/to_csv.rb
@@ -4,7 +4,7 @@ module ToCsv
   def to_csv(scope, options = default_csv_options)
     CSV.generate(options) do |csv|
       csv << updatable_attributes
-      scope.each do |row|
+      scope.find_each do |row|
         csv << row.attributes.values_at(*updatable_attributes)
       end
     end
