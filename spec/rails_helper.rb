900f9fee05917afaf70e15c7155d28e1765f1a99
soruma <soruma.net@gmail.com>
2020-01-04 08:02:26 +0900

    Instructions for getting the correct coverage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-SimpleCov.start 'rails'
-
-if ENV['CI'] == 'true'
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
-end
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV['RAILS_ENV'] ||= 'test'
