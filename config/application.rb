daf5613578ebc675e0f56e348e3e57872b9816d5
soruma <soruma.net@gmail.com>
2020-01-04 13:09:28 +0900

    Change application name

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ require 'csv'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module App
+module M3
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
