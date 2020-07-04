d501700f6b98484355bbecd1fb78a7ccce94131e
soruma <soruma.net@gmail.com>
2020-07-04 13:03:55 +0900

    Change notation to match the feature spec

--- spec/system/histories_spec.rb
+++ spec/system/histories_spec.rb
@@ -2,17 +2,16 @@
 
 require 'rails_helper'
 
-RSpec.describe 'Histories', type: :system, js: true do
-  before do
+feature 'Histories', js: true do
+  background do
     create_list(:account, 3)
     use = create(:use, name: 'Usually use')
     create(:account, name: 'Wallet', use: use)
   end
 
-  it 'Use appears when you select account' do
+  scenario 'Use appears when you select account' do
     visit new_history_path
     select 'Wallet', from: 'history[account_id]'
-    sleep 1
 
     expect(find('#use_name').text).to eq 'Usually use'
   end
