83ca4983863a3be3c7951a4e6add9d8bc46d5eb0
soruma <soruma.net@gmail.com>
2020-07-04 12:41:19 +0900

    Retirement selenium container
    This changes is temporary. This changes passes the spec.

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,7 @@ ENV LANG C.UTF-8
 RUN set -x \
     && curl -sL https://deb.nodesource.com/setup_13.x | bash - \
     && curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add - \
+    && curl -O https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb \
     && echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list \
     && apt-get update \
     && apt-get install -y --no-install-recommends \
@@ -11,6 +12,7 @@ RUN set -x \
                           libpq-dev \
                           postgresql-client \
                           yarn \
+                          ./google-chrome-stable_current_amd64.deb \
     && apt-get autoremove \
     && apt-get clean \
     && rm -rf /var/lib/apt/lists/*
--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,12 +1,15 @@
 # frozen_string_literal: true
 
-Capybara.server_host = Socket.ip_address_list.detect(&:ipv4_private?).ip_address
-Capybara.server_port = ENV.fetch('PORT') { 3000 }
-
-Capybara.register_driver :selenium_remote do |app|
-  url = "http://#{ENV['SELENIUM_HOST']}:#{ENV['SELENIUM_POST']}/wd/hub"
-  opts = { desired_capabilities: :chrome, browser: :remote, url: url }
-  Capybara::Selenium::Driver.new(app, opts)
+Capybara.register_driver :selenium_chrome_headless do |app|
+  options = Selenium::WebDriver::Chrome::Options.new
+  [
+    'no-sandbox',
+    'headless',
+    'window-size=1400x1400',
+    'disable-gpu'
+  ].each { |arg| options.add_argument(arg) }
+  Capybara::Selenium::Driver.new(app, browser: :chrome, options: options)
 end
 
-Capybara.javascript_driver = :selenium_remote
+Capybara.javascript_driver = :selenium_chrome_headless
+Capybara.default_max_wait_time = 15
