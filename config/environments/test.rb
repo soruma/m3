2d3058c23b6e1f931f4014b4bd12e7a836e6bdf4
soruma <soruma.net@gmail.com>
2020-07-04 14:18:33 +0900

    Upgrade gems

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -17,6 +17,7 @@ Rails.application.configure do
   # end
 
   config.cache_classes = false
+  config.action_view.cache_template_loading = true
 
   # Do not eager load code on boot. This avoids loading your whole application
   # just for the purpose of running a single test. If you are using a tool that
